9c927ea855243dd0a60dcfab37a82f58fec4b106
Łukasz Korecki <lukasz@coffeesounds.com>
2011-05-25 08:27:37 +0100

    More specs

--- spec/coffee/RequestSpec.coffee
+++ spec/coffee/RequestSpec.coffee
@@ -4,8 +4,18 @@ describe 'Request Spec', ->
   it 'creates an instance of Request', ->
     expect(request).toBeDefined()
   it 'assigns root url', ->
-    expect(request.url).toBe('http://example.com')
+    expect(request.url).toBe 'http://example.com'
 
-  it 'doesnt assign auth data and creates default params', ->
-    expect(request.auth.username).toBe(undefined)
-    expect(request.auth.password).toBe(undefined)
+  describe 'no auth', ->
+    it 'doesn\'t assign auth data and creates default params', ->
+      expect(request.auth.username).toBe undefined
+      expect(request.auth.password).toBe undefined
+  describe 'with auth', ->
+    auth_request = new Request 'http://example.com', {}, { username : 'lol', password : 'wat'}
+    it 'assigns auth data and creates default params', ->
+      expect(auth_request.auth.username).toBe 'lol'
+      expect(auth_request.auth.password).toBe 'wat'
+  describe 'with headers', ->
+    headers_req = new Request '', { test : 'ing'}
+    it 'assigns request headers', ->
+      expect(headers_req.requestHeaders.test).toBe 'ing'
--- spec/javascripts/RequestSpec.js
+++ spec/javascripts/RequestSpec.js
@@ -8,9 +8,31 @@
     it('assigns root url', function() {
       return expect(request.url).toBe('http://example.com');
     });
-    return it('doesnt assign auth data and creates default params', function() {
-      expect(request.auth.username).toBe(void 0);
-      return expect(request.auth.password).toBe(void 0);
+    describe('no auth', function() {
+      return it('doesn\'t assign auth data and creates default params', function() {
+        expect(request.auth.username).toBe(void 0);
+        return expect(request.auth.password).toBe(void 0);
+      });
+    });
+    describe('with auth', function() {
+      var auth_request;
+      auth_request = new Request('http://example.com', {}, {
+        username: 'lol',
+        password: 'wat'
+      });
+      return it('assigns auth data and creates default params', function() {
+        expect(auth_request.auth.username).toBe('lol');
+        return expect(auth_request.auth.password).toBe('wat');
+      });
+    });
+    return describe('with headers', function() {
+      var headers_req;
+      headers_req = new Request('', {
+        test: 'ing'
+      });
+      return it('assigns request headers', function() {
+        return expect(headers_req.requestHeaders.test).toBe('ing');
+      });
     });
   });
 }).call(this);
