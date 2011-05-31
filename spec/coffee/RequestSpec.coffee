a98c26eeb415144cf8f9c6130c3ecf9e9f328f33
Łukasz Korecki <lukasz@coffeesounds.com>
2011-05-31 08:16:32 +0100

    Request specs

--- spec/coffee/RequestSpec.coffee
+++ spec/coffee/RequestSpec.coffee
@@ -1,8 +1,13 @@
 describe 'Request Spec', ->
   request = new Request 'http://example.com'
+  server = sinon.fakeServer.create()
+  server.respondWith 'GET', 'http://example.com/test', [ 200 , {}, 'hey']
+  server.respondWith 'GET', 'http://example.com/test_fail', [ 401, {}, 'fail']
+
 
   it 'creates an instance of Request', ->
     expect(request).toBeDefined()
+
   it 'assigns root url', ->
     expect(request.url).toBe 'http://example.com'
 
@@ -19,3 +24,39 @@ describe 'Request Spec', ->
     headers_req = new Request '', { test : 'ing'}
     it 'assigns request headers', ->
       expect(headers_req.requestHeaders.test).toBe 'ing'
+
+  describe 'request testing', ->
+    it 'makes a get request to a working path', =>
+      request.get('/test', {
+          onSuccess : (req) ->
+            expect(req.url).toBe 'http://example.com/test'
+      })
+      server.respond()
+
+    it 'makes a get request and gets proper response', =>
+      request.get('/test', {
+          onSuccess : (req) ->
+            expect(req.responseText).toBe 'hey'
+      })
+      server.respond()
+
+    it 'makes a get request to a failing path', ->
+      request.get('/test_fail', {
+          onFailure : (req) ->
+            expect(req.url).toBe 'http://example.com/test_fail'
+            expect(req.status).toBe 401
+        })
+      server.respond()
+
+    it 'gets body from a request from failing path', ->
+      request.get('/test_fail', {
+          onFailure : (req) ->
+            expect(req.responseText).toBe 'fail'
+        })
+      server.respond()
+
+    it 'calls onFailure callback if path doesnt exist', ->
+      request.get('/lolol',{
+        onFailure : (req) ->
+          expect(req.status).toBe 404
+      })
--- spec/javascripts/RequestSpec.js
+++ spec/javascripts/RequestSpec.js
@@ -1,7 +1,11 @@
 (function() {
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   describe('Request Spec', function() {
-    var request;
+    var request, server;
     request = new Request('http://example.com');
+    server = sinon.fakeServer.create();
+    server.respondWith('GET', 'http://example.com/test', [200, {}, 'hey']);
+    server.respondWith('GET', 'http://example.com/test_fail', [401, {}, 'fail']);
     it('creates an instance of Request', function() {
       return expect(request).toBeDefined();
     });
@@ -25,13 +29,55 @@
         return expect(auth_request.auth.password).toBe('wat');
       });
     });
-    return describe('with headers', function() {
+    describe('with headers', function() {
       var headers_req;
       headers_req = new Request('', {
         test: 'ing'
       });
       return it('assigns request headers', function() {
         return expect(headers_req.requestHeaders.test).toBe('ing');
+      });
+    });
+    return describe('request testing', function() {
+      it('makes a get request to a working path', __bind(function() {
+        request.get('/test', {
+          onSuccess: function(req) {
+            return expect(req.url).toBe('http://example.com/test');
+          }
+        });
+        return server.respond();
+      }, this));
+      it('makes a get request and gets proper response', __bind(function() {
+        request.get('/test', {
+          onSuccess: function(req) {
+            return expect(req.responseText).toBe('hey');
+          }
+        });
+        return server.respond();
+      }, this));
+      it('makes a get request to a failing path', function() {
+        request.get('/test_fail', {
+          onFailure: function(req) {
+            expect(req.url).toBe('http://example.com/test_fail');
+            return expect(req.status).toBe(401);
+          }
+        });
+        return server.respond();
+      });
+      it('gets body from a request from failing path', function() {
+        request.get('/test_fail', {
+          onFailure: function(req) {
+            return expect(req.responseText).toBe('fail');
+          }
+        });
+        return server.respond();
+      });
+      return it('calls onFailure callback if path doesnt exist', function() {
+        return request.get('/lolol', {
+          onFailure: function(req) {
+            return expect(req.status).toBe(404);
+          }
+        });
       });
     });
   });
