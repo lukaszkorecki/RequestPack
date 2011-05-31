906cb85e7a00bd1f48082255cee8097a1021186e
Łukasz Korecki <lukasz@coffeesounds.com>
2011-05-31 18:22:58 +0100

    oauth request

---
+++ dist/oauth_request.js
@@ -0,0 +1,90 @@
+(function() {
+  var OauthRequest;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  window.OauthRequest = OauthRequest = (function() {
+    function OauthRequest(url, urlConf, consumer) {
+      this.urlConf = urlConf != null ? urlConf : null;
+      this.consumer = consumer;
+      this.requestParams = '';
+      if (this.urlConfig == null) {
+        this.urlConf = {
+          request_token: '/request_token',
+          access_token: '/access_token',
+          authorize: '/authorize'
+        };
+      }
+      this.urlConf.root = url;
+      this.oauth = OAuth(this.consumer);
+    }
+    OauthRequest.prototype.setAccessTokens = function(oauthToken, oauthTokenSecret) {
+      return this.oauth.setAccessToken([oauthToken, oauthTokenSecret]);
+    };
+    OauthRequest.prototype.requestAuth = function(openURLFunction) {
+      var failure;
+      failure = function(data) {
+        console.log('FAILED');
+        return console.dir(data);
+      };
+      return this.oauth.get(this.urlConf.root + this.urlConf.request_token, (__bind(function(data) {
+        if (data && data.text) {
+          openURLFunction(this.urlConf.root + this.urlConf.authorize + '?' + data.text);
+          return this.requestParams = data.text;
+        } else {
+          return failure(data);
+        }
+      }, this)), fail);
+    };
+    OauthRequest.prototype.secretAndToken = function(string) {
+      var pair, res;
+      res = {};
+      pair = [];
+      string.split('&').forEach(function(data) {
+        pair = data.split('=');
+        return res[pair[0]] = res[pair[1]];
+      });
+      return res;
+    };
+    OauthRequest.prototype.userAuthorize = function(pin, callbacks) {
+      var failure, url;
+      failure = function(data) {
+        console.log('ERROR');
+        console.dir(data);
+        return callbacks.onFailure(data);
+      };
+      url = "" + this.urlConf.root + this.urlConf.access_token + "?oauth_verifier=" + pin + "&" + this.requestParams;
+      return this.oauth.get(url, (__bind(function(data) {
+        var auth;
+        auth = this.secretAndToken(data.text);
+        this.setAccessTokens(auth.oauth_token, auth.oauth_token_secret);
+        return callbacks.onSuccess(auth);
+      }, this)), failure);
+    };
+    OauthRequest.prototype.get = function(path, callbacks) {
+      return this.oauth.get(this.urlConf.root + path, callbacks.onSuccess, callbacks.onFailure);
+    };
+    OauthRequest.prototype.post = function(path, data, callbacks) {
+      return this.oauth.get(this.urlConf.root + path, data, callbacks.onSuccess, callbacks.onFailure);
+    };
+    OauthRequest.prototype["delete"] = function(path, callbacks) {
+      var url;
+      url = this.urlConf.root + path;
+      return this.oauth.request({
+        method: 'DELETE',
+        url: url,
+        success: callbacks.onSuccess,
+        failure: callbacks.onFailure
+      });
+    };
+    OauthRequest.prototype.put = function(path, callbacks) {
+      var url;
+      url = this.urlConf.root + path;
+      return this.oauth.request({
+        method: 'put',
+        url: url,
+        success: callbacks.onSuccess,
+        failure: callbacks.onFailure
+      });
+    };
+    return OauthRequest;
+  })();
+}).call(this);
---
+++ source/oauth_request.coffee
@@ -0,0 +1,70 @@
+window.OauthRequest = class OauthRequest
+  constructor: (url, @urlConf = null, @consumer) ->
+
+    @requestParams = ''
+    @urlConf = {
+      request_token : '/request_token',
+      access_token : '/access_token',
+      authorize : '/authorize' } unless @urlConfig?
+
+    @urlConf.root = url
+
+    @oauth = OAuth(@consumer)
+
+  setAccessTokens : (oauthToken, oauthTokenSecret) ->
+    @oauth.setAccessToken [oauthToken, oauthTokenSecret]
+
+  requestAuth : (openURLFunction) ->
+    failure = (data) ->
+      console.log('FAILED')
+      console.dir(data)
+
+    @oauth.get(@urlConf.root + @urlConf.request_token, ((data) =>
+      if data and data.text
+        openURLFunction @urlConf.root+@urlConf.authorize+'?'+data.text
+        @requestParams = data.text
+      else
+        failure(data)
+    ), fail)
+
+  secretAndToken : (string) ->
+    res = {}
+    pair = []
+    string.split('&').forEach (data) ->
+      pair = data.split '='
+      res[pair[0]] = res[pair[1]]
+
+    res
+
+  userAuthorize : (pin, callbacks) ->
+    failure = (data) ->
+      console.log 'ERROR'
+      console.dir data
+      callbacks.onFailure data
+
+    url = "#{@urlConf.root}#{@urlConf.access_token}?oauth_verifier=#{pin}&#{@requestParams}"
+    @oauth.get(url,(
+      (data) =>
+        auth = @secretAndToken(data.text)
+        @setAccessTokens auth.oauth_token, auth.oauth_token_secret
+        callbacks.onSuccess auth
+    ), failure)
+
+  get : (path, callbacks) -> @oauth.get @urlConf.root+path, callbacks.onSuccess, callbacks.onFailure
+  post : (path, data, callbacks) -> @oauth.get @urlConf.root+path, data, callbacks.onSuccess, callbacks.onFailure
+  delete : (path, callbacks) ->
+    url = @urlConf.root + path
+    @oauth.request {
+      method : 'DELETE',
+      url : url,
+      success : callbacks.onSuccess,
+      failure : callbacks.onFailure
+    }
+  put : (path, callbacks) ->
+    url = @urlConf.root + path
+    @oauth.request {
+      method : 'PUT',
+      url : url,
+      success : callbacks.onSuccess,
+      failure : callbacks.onFailure
+    }
