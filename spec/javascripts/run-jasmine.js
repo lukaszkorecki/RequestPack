ba863beec83cd6236b55731aa15e4a8c8c68a16c
Łukasz Korecki <lukasz@coffeesounds.com>
2011-06-02 17:50:39 +0100

    Better runner output

--- spec/javascripts/run-jasmine.js
+++ spec/javascripts/run-jasmine.js
@@ -1,27 +1,28 @@
 if (phantom.state.length === 0) {
-    if (phantom.args.length !== 1) {
-        console.log('Usage: run-jasmine.js URL');
-        phantom.exit();
-    } else {
-        phantom.state = 'run-jasmine';
-        phantom.open(phantom.args[0]);
-    }
+  if (phantom.args.length !== 1) {
+    console.log('Usage: run-jasmine.js URL');
+    phantom.exit();
+  } else {
+    phantom.state = 'run-jasmine';
+    phantom.open(phantom.args[0]);
+  }
 } else {
-    window.setInterval(function () {
-        var list, el, desc, i, j;
-        if (document.body.querySelector('.finished-at')) {
-            console.log(document.body.querySelector('.description').innerText);
-            list = document.body.querySelectorAll('div.jasmine_reporter > div.suite.failed');
-            for (i = 0; i < list.length; ++i) {
-                el = list[i];
-                desc = el.querySelectorAll('.description');
-                console.log('');
-                for (j = 0; j < desc.length; ++j) {
-                    console.log(desc[j].innerText);
-                }
-            }
-            phantom.exit();
+  window.setInterval(function () {
+    var list, el, desc, i, j;
+    if (document.body.querySelector('.finished-at')) {
+      list = document.body.querySelectorAll('.suite .failed');
+      for (i = 0; i < list.length; ++i) {
+        el = list[i];
+        desc = el.querySelectorAll('.failed');
+        console.log('');
+        for (j = 0; j < desc.length; ++j) {
+          console.log('Fail: '+ (i+1));
+          console.log(desc[j].innerText);
         }
-    }, 100);
+      }
+      console.log(document.body.querySelector('.description').innerText);
+      phantom.exit();
+    }
+  }, 100);
 }
 
